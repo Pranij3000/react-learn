b412aa4b7c11aa08330c67bd4ff5d4655bb08d60
Pranij Manandhar <pranij.makura@gmail.com>
2025-05-23 21:21:18 +0545

    minor changes

--- client/src/components/TodoDisplay.jsx
+++ client/src/components/TodoDisplay.jsx
@@ -1,39 +1,39 @@
 import "./tododisplay.scss";
 
 export default function TodoDisplay({ todos, todosList }) {
-	function handleDelete(e, todo) {
-		e.preventDefault();
-		todosList(
-			todos.filter((items) => {
-				if (items.name == todo) {
-				} else {
-					return items;
-				}
-			}),
-		);
-	}
-	function handleComplete(todo) {
-		todos.map((item) => {
-			if (item.name == todo.name) {
-				if (todo.done == true) {
-					todo.done = false;
-				} else {
-					todo.done = true;
-				}
-			}
-		});
-	}
+  function handleDelete(e, todo) {
+    e.preventDefault();
+    todosList(
+      todos.filter((items) => {
+        if (items.name == todo.name) {
+        } else {
+          return items;
+        }
+      })
+    );
+  }
+  function handleComplete(todo) {
+    const updatedTodos = todos.map((item) => {
+      if (item.name === todo.name) {
+        return { ...item, done: !item.done };
+      } else {
+        return item;
+      }
+    });
+    todosList(updatedTodos);
+  }
 
-	return (
-		<>
-			<div className="todos-wrapper d-flex flex-column align-items-center">
-				{todos.map((todo, index) => (
-					<div key={index} className="todo d-flex justify-content-between align-items-center">
-						<p onClick={() => handleComplete(todo)}>{todo.name}</p>
-						<button onClick={() => handleDelete(todo)}>delete</button>
-					</div>
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="todos-wrapper d-flex flex-column align-items-center">
+        {todos.map((todo, index) => (
+          <div key={index} className={"todo d-flex justify-content-between align-items-center"}>
+            <p onClick={() => handleComplete(todo)}>{todo.name}</p>
+            <button onClick={(e) => handleDelete(e, todo)}>delete</button>
+            {console.log(todo.done)}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
