32d1f29df86dde379aca8aac1d84275549366a26
Pranij Manandhar <pranij.makura@gmail.com>
2025-05-26 18:14:33 +0545

    game created

--- client/src/TicTacToe.jsx
+++ client/src/TicTacToe.jsx
@@ -1,9 +1,11 @@
 import TttHeader from "./sections/ttt/TttHeader";
+import TttGame from "./sections/ttt/TttGame";
 
 export default function TicTacToe() {
 	return (
 		<>
 			<TttHeader></TttHeader>
+			<TttGame></TttGame>
 		</>
 	);
 }
---
+++ client/src/sections/ttt/TttGame.jsx
@@ -0,0 +1,13 @@
+export default function TttGame() {
+	return (
+		<>
+			<section className="game">
+				<div className="container">
+					<div className="game-wrapper">
+						<p>asdas</p>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
